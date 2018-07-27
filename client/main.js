f85f940cb1c561d080e3ba406b734f4fe73887fb
justbraf <dwaynecorey+github@gmail.com>
2018-07-27 02:01:26 -0400

    add modal

--- client/main.js
+++ client/main.js
@@ -1,2 +1,21 @@
-console.log(`Greetings from ${module.id}!`);
-console.log(profileData.find().count());
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import '../lib/collections.js'
+import './main.html';
+
+Template.addProfile.events({
+	'click .addbtn'(event, instance){
+		var profURL = $("#addModal input[name='proURL']").val();
+		var profFirst = $("#addModal input[name='proFirst']").val();
+		var profLast = $("#addModal input[name='proLast']").val();
+		var profSex = $("#addModal input[name='sex']").val();
+		console.log(profURL, profFirst, profLast, profSex);
+
+		$("#addModal input[name='proURL']").val("");
+		$("#addModal input[name='proFirst']").val("");
+		$("#addModal input[name='proLast']").val("");
+		$("#addModal #male").attr("checked", "checked");
+		$("#addModal").modal("hide");
+	}
+});
